d6e3bd48585a24110188088f240228dab14b20be
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-01-02 12:06:43 -0500

    Create Computer.java

---
+++ Computer.java
@@ -0,0 +1,16 @@
+public class Computer {
+  private int computerRow;
+  private int computerSticks;
+  public int getCompRow() {
+    return computerRow;
+  }
+  public int getCompSticks() {
+    return computerSticks;
+  }
+  public void setCompRow(int row) {
+    computerRow = row;
+  }
+  public void setCompSticks(int sticks) {
+    computerSticks = sticks;
+  }
+}
