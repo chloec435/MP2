d5766b29ec3c4ca1380412648a7e19217cfda03c
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-01-10 12:03:46 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 public class Game{
-  private Matchsticks matchsticks = new Matchsticks();
+  private Matchsticks matchsticks;
   private User user;
   private Computer computer;
   private int numSticks = 21;
@@ -7,61 +7,48 @@ public class Game{
   private int sticks;
   public Game() {
     instructions();
-    startingLayout();
-    System.out.println();
     play();
   }
-  public void instructions() {
+  public static void instructions() {
     System.out.println("Welcome to the game of 21 Matchsticks!");
     System.out.println("You will be playing against the computer.");
     System.out.println("The game starts with 21 matchsticks, separated into 6 rows.");
     System.out.println("Depending on how many sticks are in each row, you and the computer will take turns picking up 1, 2, or 3 matchsticks.");
     System.out.println("The player who picks up the last matchstick loses.");
-    System.out.println("Let's begin!");
     System.out.println();
-  }
-  public void startingLayout() {
-    String[] layout = {"Row 1:      |", 
-                       "Row 2:     | |", 
-                       "Row 3:    | | |", 
-                       "Row 4:   | | | |", 
-                       "Row 5:  | | | | |", 
-                       "Row 6: | | | | | |"};
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < layout.length; i++) {
-      sb.append(layout[i]);
-      if (i < layout.length - 1) {
-        sb.append("\n");
-      }
-    }
-    System.out.println(sb.toString());
+    System.out.println("Let's begin!");
   }
   public void play() {
+    matchsticks = new Matchsticks();
+    matchsticks.layoutMatchsticks(6, 0);
     while (numSticks > 0) {
-      userTurn();
+      userTurn(matchsticks);
       numSticks -= sticks;
+      System.out.println("There are " + numSticks + " matchsticks left.");
       if (numSticks == 0) {
         System.out.println("You lose!");
         break;
       }
-      matchsticks.layoutMatchsticks(row, sticks);
-      // computerTurn();
-      // numSticks -= sticks;
-      // if (numSticks == 0) {
-      //   System.out.println("You win!");
-      //   break;
-      // }
-      // matchsticks.layoutMatchsticks(row, sticks);
+      System.out.println("\nIt is now COMPUTER's turn.\n");
+      computerTurn(matchsticks);
+      numSticks -= sticks;
+      System.out.println("There are " + numSticks + " matchsticks left.");
+      if (numSticks == 0) {
+        System.out.println("You win!");
+        break;
+      }
     }
   }
-  public void userTurn() {
-    user = new User(matchsticks);
+  public void userTurn(Matchsticks m) {
+    user = new User(m);
     row = user.getUserRow();
     sticks = user.getUserSticks();
+    matchsticks.layoutMatchsticks(row, sticks);
   }
-  public void computerTurn() {
-    computer = new Computer();
+  public void computerTurn(Matchsticks m) {
+    computer = new Computer(m);
     row = computer.getCompRow();
     sticks = computer.getCompSticks();
+    matchsticks.layoutMatchsticks(row, sticks);
   }
 }
