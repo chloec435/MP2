7ccc8f7597f0d1c7a9b3d8263c2ac59e1997c287
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-01-10 12:04:12 -0500

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,16 +1,30 @@
 public class Computer {
   private int computerRow;
   private int computerSticks;
+  public Computer(Matchsticks m) {
+    System.out.println(chooseRow(m));
+    System.out.println(takeSticks(m));
+  }
+  public String chooseRow(Matchsticks m) {
+    while (true) {
+      computerRow = (int)(Math.random() * 6) + 1;
+      if (m.totalSticksLeft(computerRow, 0) > 0) {
+        return "COMPUTER chooses row " + computerRow + ".";
+      }
+    }
+  }
+  public String takeSticks(Matchsticks m) {
+    while (true) {
+      computerSticks = (int)(Math.random() * 3) + 1;
+      if (computerSticks <= m.totalSticksLeft(computerRow, 0)) {
+        return "COMPUTER takes " + computerSticks + " sticks.";
+      }
+    }
+  }
   public int getCompRow() {
     return computerRow;
   }
   public int getCompSticks() {
     return computerSticks;
-  }
-  public void setCompRow(int row) {
-    computerRow = row;
-  }
-  public void setCompSticks(int sticks) {
-    computerSticks = sticks;
   }
 }
